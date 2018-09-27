3f60ecc423aceb09511876b2efd8256496c5e1a0
oleggge <levsharu2006@gmail.com>
2018-09-27 17:52:05 +0300

    add API weather

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,78 @@
 <template>
-    <div>GGGGGGGGG</div>
+    <div>
+			<h1>{{Math.floor(temp - 273.15) }}</h1>
+			<h2 :class="{active: font === 'Times'}"  @click="changeFont">{{city}}</h2>
+			<input v-model="currentCity"/>
+			<div class="button" @click="getWeather">
+				Отправить		
+			</div>
+		</div>
 </template>
 
 <script>
 export default {
-    name: 'home'
+	name: 'home',
+	methods: {
+	getWeather() {
+		return this.$http.get(`http://api.openweathermap.org/data/2.5/weather?q=${this.currentCity}&appid=15bdf9aeed4c92e42b9b118a03740b4e`)
+		.then((response) => {
+			this.city = response.body.name
+			this.temp = response.body.main.temp
+		})
+	},
+
+	changeFont () {
+		if (this.font === '') {
+			this.font = 'Times'
+		} else {
+			this.font = ''
+		}
+	}
+},
+	data() {
+		return {
+		city: '',
+		temp: '',
+		currentCity: null,
+		font: 'Times'
+		}
+	},
+	mounted() {
+
+		this.$getLocation()
+		.then(coordinates => {
+			console.log(coordinates)
+			return coordinates
+			}).then(location => {
+			const {lat ,lng }=location 
+			return this.$http.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=15bdf9aeed4c92e42b9b118a03740b4e`)
+			} )
+		.then((response) => {
+			console.log('wefsdg', response)
+			this.city = response.body.name
+			this.temp = response.body.main.temp
+		})
+
+		
+	}
 }
+ 
 </script>
+<style>
+.active{
+	font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+
+.button {
+	margin-top: 10px;
+	text-align: center;
+	border-radius: 3px;
+	opacity: 0.7;
+	width: 100px;
+	padding: 5px;
+	color:white;
+	background-color: blue;
+
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
+import VueGeolocation from 'vue-browser-geolocation'
+
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
 Vue.use(VueResource)
+Vue.use(VueGeolocation)
 
 import Home from './components/Home.vue'
 import Blog from './components/Blog.vue'
